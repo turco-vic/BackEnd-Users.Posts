f7b411f2a4322402ba93763dd01c7f9b1ae1a4c3
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-02-18 11:52:57 -0300

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 const { v4: uuid4 } = require('uuid');
 class User {
     constructor(name, email, age) {
-        this.id = uuid();
+        this.id = uuid4();
         this.name = name;
         this.email = email;
         this.age = age;
