783200b5f521814de2df84b29119b2454d1cd2c9
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-02-18 11:14:23 -0300

    Update UserList.js

--- src/models/UserList.js
+++ src/models/UserList.js
@@ -10,4 +10,22 @@ class UserList {
     getAllUsers() {
         return this.users;
     }
+
+    getUserById(id) {
+        const user = this.users.find(user => user.id == id);
+        if (!user) throw new Error("Usuário não encontrado");
+        return user;
+    }
+
+    updateUser(id, updatedData) {
+        const user = this.getUserById(id);
+        Object.assign(user, updatedData);
+        return user;
+    }
+
+    deleteUser(id) {
+        this.users = this.users.filter(user => user.id != id);
+    }
 }
+
+module.exports = UserList;
