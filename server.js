b1d829aef5d9803437c385f74f9404ba7f57f90d
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-02-18 11:05:25 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+});
