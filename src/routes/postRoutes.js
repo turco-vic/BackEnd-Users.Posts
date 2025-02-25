67de21c84827c01bcdf756134bdcbaa19bbb3ba4
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-02-25 10:26:55 -0300

    Update postRoutes.js

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
-router.get("/", postController.getAllPosts);
-router.post("/", postController.addPost);
-router.get("/:id", postController.getPostById);
-router.put("/:id", postController.updatePost);
-router.delete("/:id", postController.deletePost);
+router.get("/post", postController.getAllPosts);
+router.post("/post", postController.addPost);
+router.get("/post:id", postController.getPostById);
+router.put("/post:id", postController.updatePost);
+router.delete("/post:id", postController.deletePost);
 
 module.exports = router;
