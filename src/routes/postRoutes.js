db3819a0fe9c526592a2216ded6dfe0b816839bd
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-04-22 08:52:48 -0300

    Update PostRoutes.js - Swagger

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,10 +4,122 @@ const postController = require("../controllers/postController");
 const apiKeyMiddleware = require("../config/apiKey");
 
 router.use(apiKeyMiddleware);
+/**
+ * @swagger
+ * tags:
+ *   name: Usúarios
+ *   description: Gerenciamento de usúarios
+ */
+
+/**
+ * @swagger
+ * /api/posts:
+ *   get:
+ *     summary: Lista todos os posts
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: query
+ *         name: name
+ *         schema:
+ *           type: string
+ *         description: Filtro por nome
+ *     responses:
+ *       200:
+ *         description: Lista de Posts!
+ */
 router.get("/", postController.getAllPosts);
+
+/**
+ * @swagger
+ * /api/posts/{id}:
+ *   get:
+ *     summary: Busca post por ID
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Post encontrado!
+ *       404:
+ *         description: Post não encontrado!
+ */
 router.get("/:id", postController.getPostById);
+
+/**
+ * @swagger
+ * /api/posts:
+ *   post:
+ *     summary: Cria um novo post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               house_id:
+ *                 type: integer
+ *               photo:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       201:
+ *         description: Post criado!
+ */
 router.post("/", postController.createPost);
+
+/**
+ * @swagger
+ * /api/posts/{id}:
+ *   put:
+ *     summary: Atualiza um post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               house_id:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Post atualizado!
+ */
 router.put("/:id", postController.updatePost);
+
+/**
+ * @swagger
+ * /api/posts/{id}:
+ *   delete:
+ *     summary: Deleta um post
+ *     tags: [Posts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Post deletado!
+ */
 router.delete("/:id", postController.deletePost);
 
 module.exports = router;
